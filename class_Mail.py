fa309b7295a20af11b71f81e16b71a6d88d9eaf0
Romain Feron <romain.feron@evobio.eu>
2015-10-06 19:00:14 +0200

    Commit Romain 2015-10-06 Modification du parser pour reconnaître les langages et implementation de correction pour certains bugs d'emails: - Emails headers sur lesquels un "=" manque à la fin - Dates contenant la time zone en lettres en plus de chiffres (eg +0200 (CET)) - Bodies vides (en réalité composés de 3 newline characters...) - Stupides emails headers encodés avec différents formats

--- class_Mail.py
+++ class_Mail.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import datetime
 import email.header
+from langdetect import detect
+import os
 
 """La classe Mail est dédiée au stockage des informations contenue
  dans chaque email.
@@ -13,6 +18,7 @@ class Mail(object):
     sender = ''  # Envoyeur de l'email
     date = ''  # Date de l'email
     body = ''  # Contenu de l'email
+    lang = 'fr' # Langage de l'email (français par défaut)
 
     # Crée un objet Mail à partir d'un fichier f
     def __init__(self, f):
@@ -35,25 +41,50 @@ class Mail(object):
                     tempSubject = tempSubject[1:len(tempSubject) - 1]
                     tempSubject = email.header.decode_header(tempSubject)
                     for s, e in tempSubject:
-                        self.subject += s.decode('utf - 8')
+                        if e != None:
+                            self.subject += s.decode(e)
+                        else:
+                            if s[0:1]!='=?':
+                                self.subject += str(s)
+                            else:
+                                temp=s+'='
+                                temp2=email.header.decode_header(temp)
+                                self.subject+= temp2[0].decode(temp2[1])
                     bodyFound = True
-
                 # Récupère la ligne contenant l'envoyeur et formate le resultat
                 # (récupère seulement l'adresse mail contenue entre les '<>')
                 if line.startswith('From: '):
                     self.sender = line[line.find('<') + 1:len(line) - 2]
                 # Récupère la ligne contenant la date et formate le résultat en
                 # un objet datetime
-                if line.startswith('Date: '):
-                    s = line[6:len(line) - 1].replace(" ", "")
+                if line.startswith('Date: ') and bodyFound==False:
+                    s = line[6:].replace(" ", "")
+                    if s.find('+') != -1:
+                        s = s[:s.find('+')+5]
+                    else:
+                        s = s[:s.find('-')+5]
                     s = s.replace(",", "")
                     s = s.replace(":", "")
                     self.date = datetime.datetime.strptime(
                         s, "%a%d%b%Y%H%M%S%z")
+        #Trouve le langage de l'email avec la lib langdetect
+        if len(self.body) > 10:
+            self.lang = detect(self.body)
 
 # Exemples d'utilisations de la classe
-testMail = Mail('Documents/bioinfo_2014-01/2.recoded')
-print('Subject: ', testMail.subject)
-print('From: ', testMail.sender)
-print('Date: ', testMail.date)
-print('Body:', testMail.body)
+#~ testMail = Mail('Documents/bioinfo_2014-04/1.recoded')
+#~ print('Subject: ', testMail.subject)
+#~ print('From: ', testMail.sender)
+#~ print('Date: ', testMail.date)
+#~ print('Body:', testMail.body)
+#~ print('Language:', testMail.lang)
+
+#Juste un test pour voir si tous les fichiers mails pouvaient bien
+# être ouverts et stockés dans une liste (prend quasiment pas de mémoire)
+listMails = []
+for root, directories, filenames in os.walk('Documents'):
+    for f in filenames:
+        if 'recoded' in f:
+            #print(os.path.join(root,f))
+            listMails.append(Mail(os.path.join(root,f)))
+
--- tests_archives.py
+++ tests_archives.py
@@ -1,17 +1,69 @@
+# -*- coding: utf-8 -*-
 import os
+from langdetect import detect
+import email.header
 
+#Basic loop on all documents checking if language worked
+with open('Documents/bioinfo_2014-02/1.recoded', encoding="utf-8") as mail:
+    for root, directories, filenames in os.walk('Documents'):
+    for f in filenames:
+        if 'recoded' in f:
+            testMail ''= Mail(os.path.join(root,f))
+            print(os.path.join(root,f))
+            if testMail.lang != 'fr':
+                print(root+'/'+f+'/'+testMail.lang)
 
-""" Petit test en cours : les mails sont encodés dans des formats différents (certains en UTF-8 et certains ont en plus d'autres caractères dans d'autres formats... Cette boucle ouvre tous les emails les uns après les autres, pour voir là où il y a des erreurs)
-  Solution possible: ouvrir le fichier en bytes (mode binaire) ? """
+#Basic test on subject parsing from only one file
+f='Documents/bioinfo_2014-04/1.recoded'
+subject=''
+with open(f, encoding="utf-8") as mail:
+    for line in mail:
+        if line.startswith('Subject: '):
+            tempSubject = line
+            # Enleve tous les tags de l'email a partir de la balise ']'
+            while tempSubject.find(']') != -1:
+                tempSubject = tempSubject[
+                    tempSubject.find(']') + 1:]
+            tempSubject = tempSubject[1:len(tempSubject) - 1]
+            tempSubject = email.header.decode_header(tempSubject)
+            print(tempSubject)
+            for s, e in tempSubject:
+                if e != None:
+                    print(e)
+                    subject += s.decode('utf - 8')
+                else:
+                    subject += s
+            print(subject)
 
-k=0
-
+#Test on subject parsing for all the files
 for root, directories, filenames in os.walk('Documents'):
-  for f in filenames:
-    if 'recoded' in f:
-      print(f)
-      with open(os.path.join(root,f), encoding="utf-8") as mail:
-        for line in mail:
-          #print(line)
-          k+=1
+    for f in filenames:
+        if 'recoded' in f:
+            print(os.path.join(root,f))
+            subject=''
+            with open(os.path.join(root,f), encoding="utf-8") as mail:
+                for line in mail:
+                    if line.startswith('Subject: '):
+                        tempSubject = line
+                        # Enleve tous les tags de l'email a partir de la balise ']'
+                        while tempSubject.find(']') != -1:
+                            tempSubject = tempSubject[
+                                tempSubject.find(']') + 1:]
+                        tempSubject = tempSubject[1:len(tempSubject) - 1]
+                        tempSubject = email.header.decode_header(tempSubject)
+                        for s, e in tempSubject:
+                            if e != None:
+                                subject += s.decode(e)
+                            else:
+                                if s[0:1]!='=?':
+                                    subject += str(s)
+                                else:
+                                    temp=s+'='
+                                    temp2=email.header.decode_header(temp)
+                                    subject+= temp2[0].decode(temp2[1])
+                        print(subject)
 
+#email decoder test
+x="CDI =?WINDOWS-1252?Q? ?= Chef de Projet en analyse de =?WINDOWS-1252?Q?données?= =?WINDOWS-1252?Q?génomiques?"
+y=email.header.decode_header(x)
+print(y)
