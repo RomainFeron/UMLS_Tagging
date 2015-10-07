9023ee1a04289f5e1f29783d58516a6fb60b97d3
RomainFeron <romain.feron@evobio.eu>
2015-10-07 23:55:30 +0200

    Formatting PEP8
    Juste quelques changements de syntaxe pour se conformer aux normes PEP8

--- class_Mail.py
+++ class_Mail.py
@@ -18,7 +18,7 @@ class Mail(object):
     sender = ''  # Envoyeur de l'email
     date = ''  # Date de l'email
     body = ''  # Contenu de l'email
-    lang = 'fr' # Langage de l'email (français par défaut)
+    lang = 'fr'  # Langage de l'email (français par défaut)
 
     # Crée un objet Mail à partir d'un fichier f
     def __init__(self, f):
@@ -41,15 +41,15 @@ class Mail(object):
                     tempSubject = tempSubject[1:len(tempSubject) - 1]
                     tempSubject = email.header.decode_header(tempSubject)
                     for s, e in tempSubject:
-                        if e != None:
+                        if e is not None:
                             self.subject += s.decode(e)
                         else:
-                            if s[0:1]!='=?':
+                            if s[0:1] != '=?':
                                 self.subject += str(s)
                             else:
-                                temp=s+'='
-                                temp2=email.header.decode_header(temp)
-                                self.subject+= temp2[0].decode(temp2[1])
+                                temp = s + '='
+                                temp2 = email.header.decode_header(temp)
+                                self.subject += temp2[0].decode(temp2[1])
                     bodyFound = True
                 # Récupère la ligne contenant l'envoyeur et formate le resultat
                 # (récupère seulement l'adresse mail contenue entre les '<>')
@@ -57,34 +57,35 @@ class Mail(object):
                     self.sender = line[line.find('<') + 1:len(line) - 2]
                 # Récupère la ligne contenant la date et formate le résultat en
                 # un objet datetime
-                if line.startswith('Date: ') and bodyFound==False:
+                if line.startswith('Date: ') and not bodyFound:
                     s = line[6:].replace(" ", "")
                     if s.find('+') != -1:
-                        s = s[:s.find('+')+5]
+                        s = s[:s.find('+') + 5]
                     else:
-                        s = s[:s.find('-')+5]
+                        s = s[:s.find('-') + 5]
                     s = s.replace(",", "")
                     s = s.replace(":", "")
                     self.date = datetime.datetime.strptime(
                         s, "%a%d%b%Y%H%M%S%z")
-        #Trouve le langage de l'email avec la lib langdetect
+        # Trouve le langage de l'email avec la lib langdetect
         if len(self.body) > 10:
             self.lang = detect(self.body)
 
 # Exemples d'utilisations de la classe
-#~ testMail = Mail('Documents/bioinfo_2014-04/1.recoded')
-#~ print('Subject: ', testMail.subject)
-#~ print('From: ', testMail.sender)
-#~ print('Date: ', testMail.date)
-#~ print('Body:', testMail.body)
-#~ print('Language:', testMail.lang)
+# testMail = Mail('Documents/bioinfo_2014-04/1.recoded')
+# print('Subject: ', testMail.subject)
+# print('From: ', testMail.sender)
+# print('Date: ', testMail.date)
+# print('Body:', testMail.body)
+# print('Language:', testMail.lang)
 
-#Juste un test pour voir si tous les fichiers mails pouvaient bien
+# Juste un test pour voir si tous les fichiers mails pouvaient bien
 # être ouverts et stockés dans une liste (prend quasiment pas de mémoire)
 listMails = []
 for root, directories, filenames in os.walk('Documents'):
     for f in filenames:
         if 'recoded' in f:
-            #print(os.path.join(root,f))
-            listMails.append(Mail(os.path.join(root,f)))
+            # print(os.path.join(root,f))
+            listMails.append(Mail(os.path.join(root, f)))
 
+print(len(listMails))
