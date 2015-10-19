5bcab6c31a3d0593d7f1e45d296f85e3374de7d8
wil <w.poudroux@gmail.com>
2015-10-19 17:35:20 +0200

    Changement de disque, adaptation de la structure des repertoires pour lisibilite

---
+++ formatThesaurus.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+l = []
+with open('extract.txt', 'r', encoding='utf-8') as fichier:
+
+print(crazy[152])
+print('CUI : ' + test.cui)
+print('LANGUAGE : ' + test.lat)
+print('STRING : ' + test.string)
