5c2c3d87349512f890f0f9b9f55edf8e3325580b
issa <diop.seydina@laposte.net>
2015-10-20 00:35:50 +0200

    stopw.py

--- stopw.py
+++ stopw.py
@@ -1,39 +1,28 @@
 
 # -*- coding: utf-8 -*-
-import codecs
 
+import codecs
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-def delete_stopW (mail_in= 'bodymail_test.txt'): #stop_wlist= 'French.txt')
-    # sw = open ('French.txt', 'r')
+'''Cette fonction prend comme entrée un bodymail, le tokenise, enlève les stopwords et renvoie une liste de mots importants stocké dans une liste'''
 
-    # with open ('stop_wlist') as sw:
-    # with open ('bodymail_test.txt', ) as bodymail:
+def delete_stopW (mail_in= 'bodymail_test.txt'): # Argument à changer en fonction de votre mail à tester
+
     bodymail = codecs.open ('bodymail_test.txt', 'r', "utf-8")
     sw = stopwords.words("french")
-    # with open ('mail_out') as mail:
     mail_f = ''
     for word in bodymail:
-        mail_f = mail_f+word
-        word_list = word_tokenize(mail_f)
-        for w in word_list:
+        mail_f = mail_f + word
+        # pont = ['.',',',';','(',')']
+        WD = word_tokenize(mail_f)
+        My_Word_list = ''
+        for w in WD:
             if w not in sw:
-                print 'mot important :', w
-            # # print w
-            # tag = nltk.pos_tag(word_list)
-            # if
-            # word_list=[]
-            # print word
-            # for w in mail_f.split():
-            #     # print w
-            #     if w not in sw:
-            #         word_list.append(w)
-            #         print (word_list)
+                My_Word_list = My_Word_list+w
+                # print 'mot important :', w
+
     return
     bodymail.close()
 
-
-# stop_wlist = 'French.txt'
-# mail_in = 'bodymail_test.txt'
 delete_stopW ('bodymail_test.txt')
