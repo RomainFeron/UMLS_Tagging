6ef08883652b94074b3d10c4ea8d529637108c24
issa <diop.seydina@laposte.net>
2015-10-19 23:29:52 +0200

    stopw.py

--- stopw.py
+++ stopw.py
@@ -3,27 +3,35 @@
 import codecs
 
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 def delete_stopW (mail_in= 'bodymail_test.txt'): #stop_wlist= 'French.txt')
     # sw = open ('French.txt', 'r')
 
     # with open ('stop_wlist') as sw:
-    with open ('bodymail_test.txt', ) as bodymail:
-    # bodymail = codecs.open ('bodymail_test.txt', 'r', "utf-8")
+    # with open ('bodymail_test.txt', ) as bodymail:
+    bodymail = codecs.open ('bodymail_test.txt', 'r', "utf-8")
     sw = stopwords.words("french")
-        # with open ('mail_out') as mail:
+    # with open ('mail_out') as mail:
     mail_f = ''
     for word in bodymail:
         mail_f = mail_f+word
-        word_list=[]
-        # print word
-        for w in mail_f.split():
-            print w
+        word_list = word_tokenize(mail_f)
+        for w in word_list:
             if w not in sw:
-                word_list.append(w)
-                # print (word_list)
+                print 'mot important :', w
+            # # print w
+            # tag = nltk.pos_tag(word_list)
+            # if
+            # word_list=[]
+            # print word
+            # for w in mail_f.split():
+            #     # print w
+            #     if w not in sw:
+            #         word_list.append(w)
+            #         print (word_list)
     return
-    sw.close()
+    bodymail.close()
 
 
 # stop_wlist = 'French.txt'
