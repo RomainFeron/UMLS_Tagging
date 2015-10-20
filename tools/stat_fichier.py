5ccedb5cc50295973926e4e8ec430ad688487998
NastroTFG <nastro_tfg@vivaldi.net>
2015-10-20 18:03:10 +0200

    Ajout outils stat + rep tools + rep tst

---
+++ tools/stat_fichier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import sys
+
+
+nb_ligne = 0
+
+with open(sys.argv[1],'r',encoding='utf-8') as fh:
+    start = time.time()
+    for line in fh:
+        nb_ligne += 1
+    end = time.time()
+
+temps = end - start
+        
+print('Fichier: ' + sys.argv[1])
+print('Nombre de lignes dans le fichier: ' + str(nb_ligne))
+print('Temps mis pour le parcours: ' + str(temps))
