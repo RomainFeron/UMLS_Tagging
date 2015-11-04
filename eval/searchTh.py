4c8abff3d3eb9b08d0bc851aa700c8e6fff047f8
NastroTFG <nastro_tfg@vivaldi.net>
2015-11-04 14:41:52 +0100

    Outils recherche string ds Th sans se preoccuper casse

---
+++ eval/searchTh.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+"""Recherche d'une chaine de caractère dans le FormatedTh à"""
+
+pathTh= '../ressources/FormattedThesaurus.RRF'
+#pathTh = './fichier_tst.txt'
+
+fh = open(pathTh,'r',encoding='utf-8')
+
+query = sys.argv[1]
+res=[]
+#res = re.IGNORECASE(query, pathTh)
+for line in fh:
+#    re.IGNORECASE
+    if (re.search(query, line, re.IGNORECASE)):
+        print(line)
+
+#print(res)
